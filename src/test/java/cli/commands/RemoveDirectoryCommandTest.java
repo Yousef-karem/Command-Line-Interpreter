493b1b9e31bfcca0ba616cb51bbab7f4c96453f6
Youssef karem <yousefkarem91@gmail.com>
2024-10-28 20:51:51 +0300

    Add Junit class

---
+++ src/test/java/cli/commands/RemoveDirectoryCommandTest.java
@@ -0,0 +1,13 @@
+package cli.commands;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RemoveDirectoryCommandTest {
+
+    @Test
+    void execute() {
+
+    }
+}

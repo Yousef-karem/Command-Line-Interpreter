d6cb9c0ca2b69e43bed56cef1f69f8f51977b42c
Ahmed Elsayed <ahmedelsayed12004@gmail.com>
2024-10-29 20:20:45 +0300

    implement ls command

--- src/main/java/cli/commands/ListFilesCommand.java
+++ src/main/java/cli/commands/ListFilesCommand.java
@@ -1,8 +1,44 @@
 package cli.commands;
 
-public class ListFilesCommand implements command{
-    public void execute()
-    {
+import java.io.File;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 
+
+public class ListFilesCommand {
+    public void execute(String[] command, File currentDirectory) {
+        boolean isReversed = false;
+        boolean showAllFiles = false;
+        boolean valid = true;
+        for (int i = 1; i < command.length; i++) {
+            if (command[i].charAt(0) == '-') {
+                for (int j = 1; j < command[i].length(); j++) {
+                    if (command[i].charAt(j) == 'r')
+                        isReversed = true;
+                    else if (command[i].charAt(j) == 'a')
+                        showAllFiles = true;
+                    else {
+                        valid = false;
+                        break;
+                    }
+                }
+            } else {
+                valid = false;
+                break;
+            }
+        }
+        if (!valid) {
+            System.out.println("Invalid command");
+            return;
+        }
+        File[] files = currentDirectory.listFiles();
+        Arrays.sort(files);
+        if (isReversed) Collections.reverse(Arrays.asList(files));
+        for (File file : files) {
+            if (showAllFiles || file.getName().charAt(0) != '.') {
+                System.out.println(file.getName());
+            }
+        }
     }
 }
